199db296ee3be500b07b5529054e6219723644f7
Valentin Gauthier <valentin.gauthier@geosiris.com>
2025-12-05 16:35:24 +0100

    new exports

--- energyml-utils/example/main_test_3D.py
+++ energyml-utils/example/main_test_3D.py
@@ -6,7 +6,8 @@ import datetime
 from pathlib import Path
 import traceback
 
-from energyml.utils.data.mesh import export_obj, read_mesh_object
+from energyml.utils.data.export import export_obj, export_stl, export_vtk
+from energyml.utils.data.mesh import read_mesh_object
 from energyml.utils.epc_stream import EpcStreamReader
 from energyml.utils.exception import NotSupportedError
 
@@ -39,6 +40,19 @@ def export_all_representation(epc_path: str, output_dir: str, regex_type_filter:
                         mesh_list=mesh_list,
                         out=f,
                     )
+                    export_stl_path = path.with_suffix(".stl")
+                    with export_stl_path.open("wb") as stl_f:
+                        export_stl(
+                            mesh_list=mesh_list,
+                            out=stl_f,
+                        )
+                    export_vtk_path = path.with_suffix(".vtk")
+                    with export_vtk_path.open("wb") as vtk_f:
+                        export_vtk(
+                            mesh_list=mesh_list,
+                            out=vtk_f,
+                        )
+
                 logging.info(f"  ✓ Exported to {path.name}")
             except NotSupportedError:
                 # print(f"  ✗ Not supported: {e}")
@@ -58,8 +72,8 @@ if __name__ == "__main__":
     import logging
 
     logging.basicConfig(level=logging.DEBUG)
-    epc_file = "rc/epc/testingPackageCpp.epc"
-    # epc_file = "rc/epc/output-val.epc"
+    # epc_file = "rc/epc/testingPackageCpp.epc"
+    epc_file = "rc/epc/output-val.epc"
     # epc_file = "rc/epc/Volve_Horizons_and_Faults_Depth_originEQN.epc"
     output_directory = Path("exported_meshes") / Path(epc_file).name.replace(".epc", "_3D_export")
     # export_all_representation(epc_file, output_directory)
